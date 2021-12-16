dbb0922f3d9910fc47aeb3e5d7b3d875e7bca1e9
Andre <andreferreira@live.co.uk>
2021-12-16 23:55:52 +0000

    modal styling

--- MRizzo_Project/src/components/MerchModal.js
+++ MRizzo_Project/src/components/MerchModal.js
@@ -1,6 +1,38 @@
 import React from "react"
 
-export default function MerchModal({ isOpen, children }) {
+const MERCH_MODAL_STYLING = {
+  position: "fixed",
+  top: "2%",
+  left: "2%",
+  right: "2%",
+  bottom: "2%",
+  zIndex: 1000,
+  backgroundColor: "rgba(0,0,0,0.9)",
+  color: "black",
+  transition: "all 100ms linear",
+  opacity: 1,
+}
+const MERCH_MODAL_OVERLAY = {
+  position: "fixed",
+  top: "0",
+  left: "0",
+  right: "0",
+  bottom: "0",
+  zIndex: 1000,
+  backgroundColor: "rgba(0,0,0,0.8)",
+}
+
+export default function MerchModal({ isOpen, onClose, children }) {
   if (!isOpen) return false
-  return <div>{children}</div>
+  return (
+    <>
+      <div style={MERCH_MODAL_OVERLAY} />
+      <div className="merch-modal" style={MERCH_MODAL_STYLING}>
+        <button className="modal-close-btn" onClick={onClose}>
+          X
+        </button>
+        {children}
+      </div>
+    </>
+  )
 }
--- MRizzo_Project/src/pages/merch.js
+++ MRizzo_Project/src/pages/merch.js
@@ -6,6 +6,7 @@ import MerchModal from "../components/MerchModal"
 
 export default function Merch() {
   const [openModal, setOpenModal] = useState(false)
+  const [imgSrc, setImgSrc] = useState(null)
 
   return (
     <Layout>
@@ -17,57 +18,83 @@ export default function Merch() {
             src="https://www.storefrontier.com/sites/default/files/product_mockups/66357/rizzo_7_170032_1083_325093_470_black170746_nobg.png?1638400254"
             loading="lazy"
             alt=""
-            onClick={() => setOpenModal(true)}
+            onClick={e => {
+              setImgSrc(e.target.src)
+              setOpenModal(true)
+            }}
           />
           <StaticImage
             src="https://www.storefrontier.com/sites/default/files/styles/product_listing_large/public/product_mockups/66357/rizzo_1_155452_1083_324971_470_black155810_nobg.png?itok=ljd-UQGE"
             loading="lazy"
             alt=""
-            onClick={() => setOpenModal(true)}
+            onClick={e => {
+              setImgSrc(e.target.src)
+              setOpenModal(true)
+            }}
           />
           <StaticImage
             src="https://www.storefrontier.com/sites/default/files/styles/product_listing_large/public/product_mockups/66357/rizzo_2_160250_1083_324990_470_black160826_nobg.png?itok=buJkTnTz"
             loading="lazy"
             alt=""
-            onClick={() => setOpenModal(true)}
+            onClick={e => {
+              setImgSrc(e.target.src)
+              setOpenModal(true)
+            }}
           />
           <StaticImage
             src="https://www.storefrontier.com/sites/default/files/styles/product_listing_large/public/product_mockups/66357/rizzo_3_161224_1083_325007_470_black161529_nobg.png?itok=9LLiVPvo"
             loading="lazy"
             alt=""
-            onClick={() => setOpenModal(true)}
+            onClick={e => {
+              setImgSrc(e.target.src)
+              setOpenModal(true)
+            }}
           />
           <StaticImage
             src="https://www.storefrontier.com/sites/default/files/styles/product_listing_large/public/product_mockups/66357/rizzo_5_164140_1083_325064_470_black164615_nobg.png?itok=juaAOPEX"
             loading="lazy"
             alt=""
-            onClick={() => setOpenModal(true)}
+            onClick={e => {
+              setImgSrc(e.target.src)
+              setOpenModal(true)
+            }}
           />
           <StaticImage
             src="https://www.storefrontier.com/sites/default/files/styles/product_listing_large/public/product_mockups/66357/rizzo_8_171056_1083_325113_470_black171801_nobg.png?itok=AAhl2EjB"
             loading="lazy"
             alt=""
-            onClick={() => setOpenModal(true)}
+            onClick={e => {
+              setImgSrc(e.target.src)
+              setOpenModal(true)
+            }}
           />
           <StaticImage
             src="https://www.storefrontier.com/sites/default/files/styles/product_listing_large/public/product_mockups/66357/rizzo_9_172200_1083_325128_470_black172607_nobg.png?itok=kkX_x-u_"
             loading="lazy"
             alt=""
-            onClick={() => setOpenModal(true)}
+            onClick={e => {
+              setImgSrc(e.target.src)
+              setOpenModal(true)
+            }}
           />
           <StaticImage
             src="https://www.storefrontier.com/sites/default/files/styles/product_listing_large/public/product_mockups/66357/rizzo_10_173139_1083_325150_470_black173515_nobg.png?itok=m2pDW7zm"
             loading="lazy"
             alt=""
-            onClick={() => setOpenModal(true)}
+            onClick={e => {
+              setImgSrc(e.target.src)
+              setOpenModal(true)
+            }}
           />
         </div>
 
-        <MerchModal isOpen={openModal}>
-          <button>X</button>
-          <StaticImage src="" alt="" />
-          <h3>title</h3>
-          <p>description</p>
+        <MerchModal isOpen={openModal} onClose={() => setOpenModal(false)}>
+          <img src={imgSrc} alt="" style={{ width: "100%" }} />
+          {/* <div className="modal-description-wrapper">
+            <h3>title: </h3>
+            <p>price: </p>
+          </div> */}
+          <button className="purchase-btn">purchase</button>
         </MerchModal>
 
         <div className="frontier-shop">
