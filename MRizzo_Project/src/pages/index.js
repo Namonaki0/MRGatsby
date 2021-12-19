1e88388a4b9c72aef25dec2901f6606cdda3d8eb
Andre <andreferreira@live.co.uk>
2021-12-19 00:39:26 +0000

    responsiveness

--- MRizzo_Project/src/pages/index.js
+++ MRizzo_Project/src/pages/index.js
@@ -1,4 +1,13 @@
 import * as React from "react"
+import {
+  BsSpotify,
+  BsYoutube,
+  BsApple,
+  BsFacebook,
+  BsInstagram,
+  BsTwitter,
+} from "react-icons/bs"
+import { FaDeezer } from "react-icons/fa"
 // import { Link } from "gatsby"
 // import { StaticImage } from "gatsby-plugin-image"
 
@@ -25,6 +34,58 @@ const IndexPage = () => (
           metal band Soulfly, formerly of ill Nino and on the rise solo projects
           Acoustic Vendetta and Revenge Beast.
         </p>
+      </div>
+      <div className="bio-social-media">
+        <a
+          href="https://open.spotify.com/artist/4MzKbulWnDVjGnMDMz1g8j?si=Cw7D7ZOHT82IxqUa79OBLQ"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsSpotify />
+        </a>
+        <a
+          href="https://www.youtube.com/c/MarcRizzoOfficial/about"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsYoutube />
+        </a>
+        <a
+          href="https://music.apple.com/us/artist/marc-rizzo/80030449"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsApple />
+        </a>
+        <a
+          href="https://deezer.page.link/LLoZB1W3Cucuj6128"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaDeezer />
+        </a>
+        <a
+          href="https://www.facebook.com/marcrizzoband/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsFacebook />
+        </a>
+        <a
+          href="https://www.instagram.com/marcrizzo_ripandshred/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsInstagram />
+        </a>
+
+        <a
+          href="https://twitter.com/rizzomarc"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsTwitter />
+        </a>
       </div>
     </div>
 
