72c4d78cf19a8ca90b5a6b27e289c4f67313497d
Andre <andreferreira@live.co.uk>
2021-12-20 23:26:46 +0000

    menu and bio social media

--- MRizzo_Project/src/components/nav.js
+++ MRizzo_Project/src/components/nav.js
@@ -1,6 +1,15 @@
 import React from "react"
 import { Link } from "gatsby"
 import { useState } from "react"
+import {
+  BsSpotify,
+  BsYoutube,
+  BsApple,
+  BsFacebook,
+  BsInstagram,
+  BsTwitter,
+} from "react-icons/bs"
+import { FaDeezer } from "react-icons/fa"
 
 export default function Nav() {
   const [openMenu, setOpenMenu] = useState(null)
@@ -66,6 +75,59 @@ export default function Nav() {
               merch
             </Link>
           </li>
+
+          <div className="nav-social-media">
+            <a
+              href="https://open.spotify.com/artist/4MzKbulWnDVjGnMDMz1g8j?si=Cw7D7ZOHT82IxqUa79OBLQ"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsSpotify />
+            </a>
+            <a
+              href="https://www.youtube.com/c/MarcRizzoOfficial/about"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsYoutube />
+            </a>
+            <a
+              href="https://music.apple.com/us/artist/marc-rizzo/80030449"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsApple />
+            </a>
+            <a
+              href="https://deezer.page.link/LLoZB1W3Cucuj6128"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaDeezer />
+            </a>
+            <a
+              href="https://www.facebook.com/marcrizzoband/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsFacebook />
+            </a>
+            <a
+              href="https://www.instagram.com/marcrizzo_ripandshred/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsInstagram />
+            </a>
+
+            <a
+              href="https://twitter.com/rizzomarc"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsTwitter />
+            </a>
+          </div>
         </ul>
 
         <div
