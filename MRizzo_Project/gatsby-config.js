5af08ff9148901a9689ac6a57c4d8b72d649edf2
Andre <andreferreira@live.co.uk>
2022-01-07 23:35:22 +0000

    twitter stream implementation

--- MRizzo_Project/gatsby-config.js
+++ MRizzo_Project/gatsby-config.js
@@ -1,13 +1,73 @@
 require("dotenv").config()
+const needle = require("needle")
 // const consumer_key = process.env.CONSUMER_KEY
 // const consumer_secret = process.env.CONSUMER_SECRET
-// const bearer_token = process.env.BEARER_TOKEN
+const bearer_token = process.env.BEARER_TOKEN
 // const twitter_end_point = "/2/tweets/1478003786887086084"
 
-// console.log(consumer_secret, consumer_key, bearer_token)
-
 const twitterRulesURL = "https://api.twitter.com/2/tweets/search/stream/rules"
-const twitterStreamURL = "https://api.twitter.com/2/tweets/search/stream"
+const twitterStreamURL =
+  "https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id"
+
+// console.log(twitterStreamURL)
+
+const rules = [{ value: "marc rizzo" }]
+
+async function getRules() {
+  const data = {
+    value: "marc rizzo",
+    id: "166445813",
+  }
+  const response = await needle("get", twitterRulesURL, data, {
+    headers: {
+      Authorization: `Bearer ${bearer_token}`,
+    },
+  })
+
+  return response.body
+}
+
+// async function setRules() {
+//   const data = {
+//     add: rules,
+//   }
+//   const response = await needle("post", twitterRulesURL, data, {
+//     headers: {
+//       "content-type": "application/json",
+//       Authorization: `Bearer ${bearer_token}`,
+//     },
+//   })
+//   console.log(response.body)
+//   return response.body
+// }
+
+function tweetStream() {
+  const stream = needle.get(twitterStreamURL, {
+    headers: {
+      Authorization: `Bearer ${bearer_token}`,
+    },
+  })
+  stream.on("data", data => {
+    try {
+      const jsonFormat = JSON.parse(data)
+      console.log(jsonFormat)
+    } catch (error) {}
+  })
+}
+
+;(async () => {
+  let currentRules
+
+  try {
+    // await setRules()
+    currentRules = await getRules()
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+
+  tweetStream()
+})()
 
 module.exports = {
   siteMetadata: {
