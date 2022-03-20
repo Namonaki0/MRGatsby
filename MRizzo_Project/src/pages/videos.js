4fd393d0b36f78dc07d3e9a09b62cc3878e23f25
Andre <andreferreira@live.co.uk>
2022-03-20 23:44:47 +0000

    videos section styling

--- MRizzo_Project/src/pages/videos.js
+++ MRizzo_Project/src/pages/videos.js
@@ -4,10 +4,16 @@ import { useState, useEffect } from "react"
 export default function Videos() {
   const baseUrl =
     " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.youtube.com%2Ffeeds%2Fvideos.xml%3Fchannel_id%3D"
+  const playlistFeed =
+    "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fyoutube.com%2Ffeeds%2Fvideos.xml%3Fplaylist_id%3D"
 
   const [videos, setVideos] = useState([])
+  const [feedVideos, setFeedVideos] = useState([])
   const [currentChannelId, setCurrentChannelId] = useState(
     "UCkNW3tA-mFGnkviFNPzTq5A"
+  )
+  const [livestreamPlaylist, setLivestreamPlaylist] = useState(
+    "PLZDs0akd6CkwHzetjWFKMWDnLolVuT47_"
   )
 
   useEffect(() => {
@@ -15,20 +21,24 @@ export default function Videos() {
       const data = await fetch(`${baseUrl}${currentChannelId}`).then(res =>
         res.json()
       )
+      const livestream = await fetch(
+        `${playlistFeed}${livestreamPlaylist}`
+      ).then(res => res.json())
 
       setVideos(data.items)
-      console.log(data)
+      setFeedVideos(livestream.items)
+      console.log(livestream)
     })()
-  }, [currentChannelId])
+  }, [currentChannelId, livestreamPlaylist])
 
   return (
     <div className="video-library-wrapper">
       <h1>VIDEOS</h1>
-
-      {videos.map(video => (
-        <div className="video-library">
-          <div key={video.guid} className="individual-video">
-            <a href={video.link} target="_blank">
+      <div className="videos-section-block">
+        <h2>Latest</h2>
+        {videos.map(video => (
+          <div key={video.guid} className="library-video">
+            <a href={video.link} target="_blank" rel="noreferrer">
               <img
                 alt="video-template"
                 src={`https://i4.ytimg.com/vi/${
@@ -36,12 +46,48 @@ export default function Videos() {
                 }/mqdefault.jpg`}
               />
             </a>
+            <div className="video-footer">
+              <p>{video.title}</p>
+            </div>
           </div>
-          <div className="video-footer">
-            <p>{video.title}</p>
+        ))}
+        <a
+          href="https://www.youtube.com/c/MarcRizzoOfficial/videos"
+          alt="more videos"
+          className="more-videos-cta"
+          target="_blank"
+          rel="noreferrer"
+        >
+          more videos
+        </a>
+      </div>
+      <div className="videos-section-block">
+        <h2>Livestreams</h2>
+        {feedVideos.map(feedVideo => (
+          <div key={feedVideo.guid} className="library-video">
+            <a href={feedVideo.link} target="_blank" rel="noreferrer">
+              <img
+                alt="video-template"
+                src={`https://i4.ytimg.com/vi/${
+                  feedVideo.guid.split(":")[2]
+                }/mqdefault.jpg`}
+              />
+            </a>
+            <div className="video-footer">
+              <p>{feedVideo.title}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+        <a
+          href="https://www.youtube.com/playlist?list=PLZDs0akd6CkwHzetjWFKMWDnLolVuT47_"
+          alt="more videos"
+          className="more-videos-cta"
+          target="_blank"
+          rel="noreferrer"
+        >
+          more videos
+        </a>
+      </div>
     </div>
   )
 }
