53da00b1a8df3b351dbf39cf32a911a8c7fd16a9
Andre <andreferreira@live.co.uk>
2022-05-07 23:30:39 +0100

    404 page layout

--- MRizzo_Project/src/pages/404.js
+++ MRizzo_Project/src/pages/404.js
@@ -6,9 +6,26 @@ import Seo from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <Seo title="404: Not found" />
-    <h1>PAGE NOT FOUND</h1>
-    <Link to="/">back to Homepage</Link>
+    <div className="page-not-found-wrapper">
+      <div className="logo-background-image"></div>
+      <Seo title="404: Not found" />
+      <p className="primary-error-message">Oops... something went wrong</p>
+      <h1>PAGE NOT FOUND</h1>
+      <div className="back-to-links-wrapper">
+        {/* <p>return to</p> */}
+        <div className="back-to-links">
+          <Link to="/" className="back-to-nav-link">
+            home
+          </Link>
+          <Link to="/videos" className="back-to-nav-link">
+            videos
+          </Link>
+          <Link to="/merch" className="back-to-nav-link">
+            merch
+          </Link>
+        </div>
+      </div>
+    </div>
   </Layout>
 )
 
